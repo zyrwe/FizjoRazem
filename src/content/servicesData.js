df2e5e9343e70c2d1e8c2705b40075bf597f3f41
zyrwe <zyrwek@gmail.com>
2026-02-20 22:41:54 +0100

    Assets

--- src/content/servicesData.js
+++ src/content/servicesData.js
@@ -1,38 +1,45 @@
+import KINEZYTERAPIA from "@/assets/services/KINEZYTERAPIA.png";
+import PINOTERAPIA from "@/assets/services/PINOTERAPIA.png";
+import KINESIOLOGYTAPING from "@/assets/services/KINESIOLOGYTAPING.png";
+import BANKI from "@/assets/services/KINEZYTERAPIA.png";
+import MASAZ from "@/assets/services/KINEZYTERAPIA.png";
+import BLIZNY from "@/assets/services/KINEZYTERAPIA.png";
+
 export const services = [
   {
     title: "KINEZYTERAPIA",
     description:
       "Terapeutyczne ćwiczenia ruchowe poprawiające sprawność i przywracające równowagę mięśniowo-stawową.",
-    image: "public/assets/services/KINEZYTERAPIA.png",
+    image: KINEZYTERAPIA,
   },
   {
     title: "PINOTERAPIA",
     description:
       "Delikatna stymulacja punktów na ciele, wspomagająca regenerację i redukcję napięć.",
-    image: "public/assets/services/PINOTERAPIA.png",
+    image: PINOTERAPIA,
   },
   {
     title: "KINESIOLOGYTAPING",
     description:
       "Specjalistyczne plastry wspierające mięśnie i stawy, poprawiające krążenie i przyspieszające regenerację.",
-    image: "public/assets/services/KINESIOLOGYTAPING.png",
+    image: KINESIOLOGYTAPING,
   },
   {
     title: "BAŃKI OGNIOWE",
     description:
       "Metoda leczenia miejscowego, pobudzająca krążenie i redukująca napięcie mięśniowe.",
-    image: "public/assets/services/KINEZYTERAPIA.png",
+    image: BANKI,
   },
   {
     title: "MASAŻ",
     description:
       "Różne techniki masażu, rozluźniające mięśnie, redukujące stres i poprawiające samopoczucie.",
-    image: "public/assets/services/KINEZYTERAPIA.png",
+    image: MASAZ,
   },
   {
     title: "TERAPIA BLIZNY",
     description:
       "Specjalistyczna pielęgnacja blizn po zabiegach lub urazach, poprawiająca elastyczność i wygląd skóry.",
-    image: "public/assets/services/KINEZYTERAPIA.png",
+    image: BLIZNY,
   },
 ];
