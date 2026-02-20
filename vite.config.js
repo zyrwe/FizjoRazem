5d273d70467ee211afa30f364ec702a79bfd0038
zyrwe <zyrwek@gmail.com>
2026-02-20 21:38:14 +0100

    Fx

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/FizjoRazem/", // Ustawienie base na "/FizjoRazem/" dla poprawnego działania na GitHub Pages
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname,
+      "@": new URL("./src", import.meta.url).pathname,
     },
   },
-  base: '/fizjorazem/',
-})
+  base: "/fizjorazem/",
+});
